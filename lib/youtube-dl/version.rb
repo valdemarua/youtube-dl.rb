25d8f065548250eb9fb6e6d919d5ad737c6771ad
valdemarua <valdemarua@valdemaruas-iMac.local>
2018-10-03 20:22:51 +0300

    Bump version to 0.3.1.2018.09.26

--- lib/youtube-dl/version.rb
+++ lib/youtube-dl/version.rb
@@ -6,5 +6,5 @@
 module YoutubeDL
   # Semantic Version as well as the bundled binary version.
   # "(major).(minor).(teeny).(pre-release).(binary-version)"
-  VERSION = '0.3.1.2018.08.04'.freeze
+  VERSION = '0.3.1.2018.09.26'.freeze
 end
