30efd9c71bbaf45d600ca55ff42796fd4ac928f7
Vova Pokropyvnyi <valdemarua@gmail.com>
2020-08-11 14:43:37 +0300

    Update binary to 2020.07.28

--- lib/youtube-dl/version.rb
+++ lib/youtube-dl/version.rb
@@ -6,5 +6,5 @@
 module YoutubeDL
   # Semantic Version as well as the bundled binary version.
   # "(major).(minor).(teeny).(pre-release).(binary-version)"
-  VERSION = '0.3.1.2020.05.08'.freeze
+  VERSION = '0.3.1.2020.07.28'.freeze
 end
